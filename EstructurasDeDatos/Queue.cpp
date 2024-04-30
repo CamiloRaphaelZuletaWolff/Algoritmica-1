fc069bb48967df5b377dd3d3887c71d5ad617dcd
CamiloRaphaelZuletaWolff <165272657+CamiloRaphaelZuletaWolff@users.noreply.github.com>
2024-04-29 23:23:21 -0400

    Update Queue.cpp

--- EstructurasDeDatos/Queue.cpp
+++ EstructurasDeDatos/Queue.cpp
@@ -1,1 +1,9 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    queue<tipo_de_Dato>nombre_De_La_Cola
+    nombre_De_La_Cola.push(Que_se_agregara);  // agregar algo a la cola 
+    nombre_De_La_Cola.empty();  // verificar si la cola esta vacia o no 
+    nombre_De_La_Cola.front();  // fijarse que esta en el "frente" de la cola 
+    nombre_De_La_Cola.pop();  // sacar al elemento de arriba de la cola
+}
