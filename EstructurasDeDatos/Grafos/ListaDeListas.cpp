26e72c195fed2b9e0b9b8305fc07f37d360697ab
CamiloRaphaelZuletaWolff <165272657+CamiloRaphaelZuletaWolff@users.noreply.github.com>
2024-04-29 23:47:01 -0400

    Update ListaDeListas.cpp

--- EstructurasDeDatos/Grafos/ListaDeListas.cpp
+++ EstructurasDeDatos/Grafos/ListaDeListas.cpp
@@ -1,1 +1,19 @@
+#include<bits/stdc++.h>
 
+using namespace std;
+
+#define input freopen("in.txt","r",stdin)
+#define output freopen("out.txt","w",stdout)
+
+int main() {
+    int numNodos, numAristas;;
+    cin >> numNodos>> numAristas; // Obtener el número de nodos y aristas del grafo.
+    vector<int> grafo[numNodos]; // Crea la estructura del grafo
+    for (int i = 0; i < numAristas; i++) {
+        int nodoOrigen, nodoDestino;
+        cin >> nodoOrigen >> nodoDestino; 
+        grafo[nodoOrigen].push_back(nodoDestino); // Añade el nodo destino al grafo en la posicion nodoOrigen
+        // Esto es dirigido, para hacerlo no dirigido tenemos que añadir 
+        grafo[nodoDestino].push_back(nodoOrigen);
+    }
+}
