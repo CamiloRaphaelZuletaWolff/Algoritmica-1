e12793b6ee27f78f7a33e20b473109ff3416f4c4
CamiloRaphaelZuletaWolff <165272657+CamiloRaphaelZuletaWolff@users.noreply.github.com>
2024-04-29 23:51:55 -0400

    Update RepresentacionMatricial.cpp

--- EstructurasDeDatos/Grafos/RepresentacionMatricial.cpp
+++ EstructurasDeDatos/Grafos/RepresentacionMatricial.cpp
@@ -1,1 +1,19 @@
+#include<bits/stdc++.h>
 
+using namespace std;
+
+#define input freopen("in.txt","r",stdin)
+#define output freopen("out.txt","w",stdout)
+
+int main() {
+    int numNodos, numAristas;;
+    cin >> numNodos>> numAristas; // Obtener el número de nodos y aristas del grafo.
+    int grafo[numNodos][numNodos]; // Crea la estructura del grafo
+    for (int i = 0; i < numAristas; i++) {
+        int nodoOrigen, nodoDestino;
+        cin >> nodoOrigen >> nodoDestino; 
+        grafo[nodoOrigen][nodoDestino] = 1; // Un 1 en la matriz implica una conexion entre esos nodos
+        // Esto es dirigido, para hacerlo no dirigido tenemos que añadir 
+        grafo[nodoDestino][nodoOrigen];
+    }
+}
