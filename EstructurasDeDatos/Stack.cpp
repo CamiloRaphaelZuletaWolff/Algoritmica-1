492fc15f33c3deef60ca630708df90dd31c87525
CamiloRaphaelZuletaWolff <165272657+CamiloRaphaelZuletaWolff@users.noreply.github.com>
2024-04-29 23:11:14 -0400

    Update Stack.cpp

--- EstructurasDeDatos/Stack.cpp
+++ EstructurasDeDatos/Stack.cpp
@@ -1,1 +1,9 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    stack<tipo_de_Dato>nombre_Del_Stack 
+    nombre_Del_Stack.push(Que_se_agregara);  // push = agregar algo a la pila 
+    nombre_Del_Stack.empty();  // verificar si la pila esta vacia o no 
+    nombre_Del_Stack.top();  // fijarse que esta en el "tope" de la pila 
+    nombre_Del_Stack.pop();  // sacar al elemento de arriba de la pila 
+} 
